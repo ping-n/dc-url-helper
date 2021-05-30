2f06eaf54ab1a83a6c9c130509a1beee18c6176e
PING NGE <ping.nge@hotmail.com>
2021-05-30 22:07:55 +1000

    UPDATE: basic styling for index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,20 @@
   <title>DC URL</title>
 </svelte:head>
 
-<h1 class="text-lg">DC URL HELPER</h1>
+<div class="container px-5 py-5 mx-auto">
+  <h1 class="text-3xl font-bold text-center text-blue-500">DC URL HELPER</h1>
+
+  <div
+    class="flex min-h-full min-w-4/5 flex-col justify-items-center text-left uppercase font-bold"
+  >
+    <div class="main-link mt-4">
+      <a href="/main">Main URL</a>
+    </div>
+    <div class="main-link">
+      <a href="/filter">Filter URL</a>
+    </div>
+    <div class="main-link">
+      <a href="/registration">Registration URL</a>
+    </div>
+  </div>
+</div>
