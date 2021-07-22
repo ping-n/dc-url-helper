24a04f1aee4edf6456868b502e6aef2527590447
PING NGE <ping.nge@hotmail.com>
2021-07-23 07:04:35 +1000

    UPDATE: footer year

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,11 @@
+<script>
+  let year = new Date().getFullYear();
+</script>
+
 <footer class="text-gray-600 body-font">
   <div class="container px-5 py-8 mx-auto flex items-center flex-col">
     <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
-      © 2021 Justin Nge
+      © {year} Justin Nge
     </p>
   </div>
 </footer>
